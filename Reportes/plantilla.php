9c86c0b786f4d4c5b106011da6365b59bd6e05d1
Dario Gonzalez <dario.gonzalez@estudiantes.utec.edu.uy>
2023-01-11 12:51:45 -0300

    added PDF reports from obtener.php

--- Reportes/plantilla.php
+++ Reportes/plantilla.php
@@ -2,17 +2,21 @@
 	// require './Librerias/fpdf/fpdf.php';
     require 'D:\Desarrollo\laragon\www\censoAnimal\Librerias\fpdf\fpdf.php';
     // require 'Librerias\fpdf\fpdf.php';
-
+	setlocale(LC_ALL, 'es_UY');
+	date_default_timezone_set('America/Montevideo');
 	// include './Librerias/fpdf/fpdf.php';
-
 	class PDF extends FPDF
 	{
+		
 		function Header()
 		{
 			$this->Image('D:\Desarrollo\laragon\www\censoAnimal\imagenes\membrete-01.png', 5, 5, 30 );
-			$this->SetFont('Arial','B',15);
+			$this->SetFont('Arial','I',10);
+			$tDate = date("d/m/Y g:i a");
+        	$this->Cell(0, 10, ''.$tDate, 0, false, 'R', 0, '', 0, false, 'T', 'M');
 			$this->Cell(30);
-			$this->Cell(120,10, 'Reporte De Propietarios',0,0,'C');
+			$this->SetFont('Arial','B',15);
+			$this->Cell(120,10, 'Reporte Censo animal',0,0,'C');
 			$this->Ln(20);
 		}
 		
